cded6946fdf536ea94576f7ed35b9a47d414877c
kennedy-ben <makuanikennedy@gmail.com>
2024-07-04 00:37:36 +0300

    button for adding tasks

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { create } from "zustand"
 import { createJSONStorage, persist } from "zustand/middleware"
-/**
- * 1. Interface IGlobalStore
- * 2. create
- * 3. persist with async from react-native-async-storage
- */
 
 interface IGlobalStore {
   categories: ICategory[]
