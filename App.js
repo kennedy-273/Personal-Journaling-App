74c2577d85c3a988a048b6e88fe33225861e19db
kennedy-ben <makuanikennedy@gmail.com>
2024-07-03 23:59:33 +0300

    continue creating category list

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-// import Button from './components/button';
-import { NavigationContainer} from '@react-navigation/native';
-import Navigation from './naviagtion';
-import  { ThemeProvider } from '@shopify/restyle';
-import { theme } from './utils/theme';
-
-
-
+import Navigation from "./naviagtion";
+import theme from "./utils/theme";
+import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from "@shopify/restyle";
+import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <ThemeProvider theme={theme}>
-      
-      <Navigation />
+    <GestureHandlerRootView
+      style={{
+        flex: 1,
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider>
+          <BottomSheetModalProvider>
+            <NavigationContainer>
+              <Navigation />
+            </NavigationContainer>
+          </BottomSheetModalProvider>
+        </SafeAreaProvider>
       </ThemeProvider>
-    </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
-
-
-
-
