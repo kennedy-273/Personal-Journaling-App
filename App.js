73b7beb0e95dca53810f904649c4c85f2304a93d
kennedy-ben <makuanikennedy@gmail.com>
2024-07-05 13:09:37 +0300

    removed font for styling

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import { useFonts as useBioRhyme, BioRhyme_400Regular } from '@expo-google-fonts/biorhyme-expanded';
-import { useFonts as useSpaceMono, SpaceMono_400Regular, SpaceMono_400Regular_Italic } from '@expo-google-fonts/space-mono';
-import { useFonts as useBigShoulders, BigShouldersDisplay_700Bold } from '@expo-google-fonts/big-shoulders-display';
-import { useFonts as useAnton, Anton_400Regular } from '@expo-google-fonts/anton';
-import AppLoading from 'expo-app-loading';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  useFonts as useBioRhyme,
+  BioRhyme_400Regular,
+} from "@expo-google-fonts/biorhyme-expanded";
+import {
+  useFonts as useSpaceMono,
+  SpaceMono_400Regular,
+  SpaceMono_400Regular_Italic,
+} from "@expo-google-fonts/space-mono";
+import {
+  useFonts as useBigShoulders,
+  BigShouldersDisplay_700Bold,
+} from "@expo-google-fonts/biorhyme-expanded";
+import {
+  useFonts as useAnton,
+  Anton_400Regular,
+} from "@expo-google-fonts/anton";
+import AppLoading from "expo-app-loading";
 
-import HomeNavigator from './HomeNavigator';
-import JournalEntry from './components/JournalEntry';
-import CalendarComponent from './components/Calendar';
-import SubmittedEntry from './components/SubmittedEntry';
+import HomeNavigator from "./HomeNavigator";
+import JournalEntry from "./components/JournalEntry";
+import CalendarComponent from "./components/Calendar";
+import SubmittedEntry from "./components/SubmittedEntry";
 
 const Tab = createBottomTabNavigator();
 
@@ -33,67 +46,74 @@ const App = () => {
     BigShouldersDisplay_700Bold,
   });
 
-  if (!antonLoaded || !bioLoaded || !spaceLoaded || !bigLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color }) => {
-              let iconName;
-              if (route.name === 'Home') {
-                iconName = 'home';
-              } else if (route.name === 'New Entry') {
-                iconName = 'add-circle';
-              } else if (route.name === 'My Journal') {
-                iconName = 'journal';
-              } else if (route.name === 'Calendar') {
-                iconName = 'calendar-outline';
-              }
-              return <Ionicons name={iconName} size={35} color={color} />;
-            },
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: colors.cobaltBlue },
-            headerTitleStyle: {
-              color: '#FFF',
-              fontFamily: fonts.Anton,
-              fontSize: 24,
-              letterSpacing: 1,
-              textTransform: 'uppercase',
-            },
-            tabBarLabelStyle: { paddingBottom: 10 },
-            tabBarStyle: { height: 80, backgroundColor: colors.midnightBlue },
-            tabBarActiveTintColor: colors.mint,
-            tabBarLabelStyle: { fontFamily: fonts.BioRhyme, paddingBottom: 7 },
-            tabBarInactiveTintColor: '#FFF',
-          })}
-        >
-          <Tab.Screen name="Home" component={HomeNavigator} />
-          <Tab.Screen name="New Entry" component={JournalEntry} />
-          <Tab.Screen name="My Journal" component={SubmittedEntry} />
-          <Tab.Screen name="Calendar" component={CalendarComponent} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
+  // if (!antonLoaded || !bioLoaded || !spaceLoaded || !bigLoaded) {
+  //   console.log(
+  //     "Loading >>>>>>>>...",
+  //     antonLoaded,
+  //     bioLoaded,
+  //     spaceLoaded,
+  //     bigLoaded
+  //   );
+  //   return <AppLoading />;
+  // } else {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => {
+            let iconName;
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "New Entry") {
+              iconName = "add-circle";
+            } else if (route.name === "My Journal") {
+              iconName = "journal";
+            } else if (route.name === "Calendar") {
+              iconName = "calendar-outline";
+            }
+            return <Ionicons name={iconName} size={35} color={color} />;
+          },
+          headerTitleAlign: "center",
+          headerStyle: { backgroundColor: colors.cobaltBlue },
+          headerTitleStyle: {
+            color: "#FFF",
+            fontFamily: fonts.Anton,
+            fontSize: 24,
+            letterSpacing: 1,
+            textTransform: "uppercase",
+          },
+          tabBarLabelStyle: { paddingBottom: 10 },
+          tabBarStyle: { height: 80, backgroundColor: colors.midnightBlue },
+          tabBarActiveTintColor: colors.mint,
+          tabBarLabelStyle: { fontFamily: fonts.BioRhyme, paddingBottom: 7 },
+          tabBarInactiveTintColor: "#FFF",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeNavigator} />
+        <Tab.Screen name="New Entry" component={JournalEntry} />
+        <Tab.Screen name="My Journal" component={SubmittedEntry} />
+        <Tab.Screen name="Calendar" component={CalendarComponent} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+  // }
 };
 
 const fonts = {
-  Anton: 'Anton_400Regular',
-  BioRhyme: 'BioRhyme_400Regular',
-  SpaceMono: 'SpaceMono_400Regular',
-  SpaceItalic: 'SpaceMono_400Regular_Italic',
-  BigShoulders: 'BigShouldersDisplay_700Bold',
+  Anton: "Anton_400Regular",
+  BioRhyme: "BioRhyme_400Regular",
+  SpaceMono: "SpaceMono_400Regular",
+  SpaceItalic: "SpaceMono_400Regular_Italic",
+  BigShoulders: "BigShouldersDisplay_700Bold",
 };
 
 const colors = {
-  pink: '#FF449F',
-  yellow: '#FFD32D',
-  mint: '#C1F8CF',
-  turquoise: '#4FD3C4',
-  midnightBlue: '#3E4985',
-  cobaltBlue: '#488FB1',
+  pink: "#FF449F",
+  yellow: "#FFD32D",
+  mint: "#C1F8CF",
+  turquoise: "#4FD3C4",
+  midnightBlue: "#3E4985",
+  cobaltBlue: "#488FB1",
 };
 
 export default App;
--- HomeNavigator.js
+++ HomeNavigator.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
 
-import Home from './components/Home'
-import Login from './components/Login'
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 const Stack = createStackNavigator();
 
 const HomeNavigator = () => {
+  console.log("HomeNavigator");
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: false
-      }}>
-      <Stack.Screen 
-        name='Home'
-        component={Home}
-      />
-      <Stack.Screen 
-        name='Login'
-        component={Login}
-      />
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 export default HomeNavigator;
